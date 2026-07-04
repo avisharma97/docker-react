6f6c5c679c041802929276d12b2a1b94a39354b9
AvinashSharma <1997avinashsharma@gmail.com>
2026-07-04 23:54:51 +0530

    Add GitHub Actions workflow f

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello avi sharma
+          Edit <code>src/App.js</code> and save to reload. Hello from github
+          actions
         </p>
         <a
           className="App-link"
